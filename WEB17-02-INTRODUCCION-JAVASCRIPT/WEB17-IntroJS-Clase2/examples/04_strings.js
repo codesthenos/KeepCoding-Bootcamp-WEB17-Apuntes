6a2cd8a1f85d18af532abb7198e200cc6705ba14
codesthenos <codesthenos@gmail.com>
2024-09-05 20:31:58 +0200

    incluyo el .toLowerCase(), metodo para convertir todos los caracters del strinng en minusculas

--- WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase2/examples/04_strings.js
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase2/examples/04_strings.js
@@ -2,13 +2,13 @@ document.querySelector('body').innerHTML = `
 <h1 style='text-align: center';>04_strings.js</h1>
 `
 
-const username = '  Guille     '
+const username = '  GuilLE     '
 const password = ' 1234   '
 let authenticated = false
 
-const sanitizedUsername = username.trim()
+const sanitizedUsername = username.trim().toLowerCase()
 
-if (sanitizedUsername === 'Guille' && password.trim() === '1234') {
+if (sanitizedUsername === 'guille' && password.trim() === '1234') {
   authenticated = true
 }
 
