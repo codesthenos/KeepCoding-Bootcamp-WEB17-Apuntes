fadd27b7726defb5d5b05f352f42acd86cc6d70a
codesthenos <codesthenos@gmail.com>
2024-09-05 20:26:54 +0200

    creado strings.js y viendo la funcion .trim()

---
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase2/examples/04_strings.js
@@ -0,0 +1,15 @@
+document.querySelector('body').innerHTML = `
+<h1 style='text-align: center';>04_strings.js</h1>
+`
+
+const username = '  Guille     '
+const password = ' 1234   '
+let authenticated = false
+
+const sanitizedUsername = username.trim()
+
+if (sanitizedUsername === 'Guille' && password.trim() === '1234') {
+  authenticated = true
+}
+
+console.log(`Is ${sanitizedUsername} authenticated: ${authenticated}`)
