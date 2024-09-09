f0bed1427793aa08f35faf1a6961645a1f250df0
codesthenos <codesthenos@gmail.com>
2024-09-09 21:38:42 +0200

    Vemos el uso del for, y ademas incluyo lo que veremos en la siguiente clase, haciendo lo que vamos a hacer ahora con el for, ocn un map

--- WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/examples/07_bucles.js
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/examples/07_bucles.js
@@ -1,4 +1,26 @@
 document.querySelector('body').innerHTML = `
-<h1 style='text-align: center';>example/07_bucles.js</h1>
+<h1 style='text-align: center';>WEB17-IntroJS-Clase3/example/07_bucles.js</h1>
 `
 
+const products = ['Cap', 'T-shirt', 'shoes']
+
+const productMessage = (productTitle) => {
+  return `Product: ${productTitle}`
+}
+
+console.log(productMessage(products[0]))
+
+// while, do-while, for
+for (let index = 0; index < 10; index++) {
+  console.log('Dentro del loop, index = ', index)
+}
+
+
+// Extra usando map
+// La funcion, coge una lista de productos, e itrera por los productos, de cada producto, hace un console.log de la template string
+const productMessageList = (productlist) =>
+  productlist.map(product =>
+    console.log(`Product: ${product}`)
+  )
+
+productMessageList(products)
