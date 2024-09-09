5216dbe189b64ef65b87ad0ed4d03840d814c842
codesthenos <codesthenos@gmail.com>
2024-09-09 21:55:07 +0200

    Incluimos un ejemplo extra para crear una funcionalidad que dependiendo de si un producto esta en una lista de hidden, mostrar 4 asteriscos en vez de el nombre del producto

--- WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/examples/07_bucles.js
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/examples/07_bucles.js
@@ -2,7 +2,7 @@ document.querySelector('body').innerHTML = `
 <h1 style='text-align: center';>WEB17-IntroJS-Clase3/example/07_bucles.js</h1>
 `
 
-const products = ['Cap', 'T-shirt', 'shoes']
+const products = ['Cap', 'T-shirt', 'shoes', 'Tv', 'Pc']
 
 const productMessage = (productTitle) => {
   return `Product: ${productTitle}`
@@ -18,10 +18,19 @@ for (let index = 1; index < 5; index++) {
     : console.log('Dentro del loop, index es impar, index = ', index)  
 }
 // usando un for para products
+// creamos lista de productos que no queremos mostrar
+const hiddenProducts = ['Tv', 'Pc']
+
 console.log('---Usando for---')
 for (let index = 0; index < products.length; index++) {
-  console.log(productMessage(products[index]))
+  // usamos la lista de hiddenProducts para esconder los productos que no queremos mostrar
+  if (hiddenProducts.includes(products[index])) {
+    console.log(productMessage('****'))
+  } else {
+    console.log(productMessage(products[index]))
+  }
 }
+
 
 // Extra usando map
 // La funcion, coge una lista de productos, e itrera por los productos, de cada producto, hace un console.log de la template string
