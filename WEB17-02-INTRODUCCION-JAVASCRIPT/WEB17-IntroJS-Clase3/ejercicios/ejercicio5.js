9ccacbc6c8ed778d8ded585c2e52cb73582fa1e4
codesthenos <codesthenos@gmail.com>
2024-09-09 22:41:02 +0200

    Termino de corregir el ejercicio y ahora ya si filtra todo, incluidos strings, no solo falsys

--- WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/ejercicios/ejercicio5.js
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase3/ejercicios/ejercicio5.js
@@ -2,19 +2,21 @@ document.querySelector('body').innerHTML = `
 <h1 style='text-align: center';>WEB17-IntroJS-Clase3/ejercicios/ejercicio5.js</h1>
 `
 
-const numbers = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101, null, '', false, NaN]
+const numbers = [1, 2, 33, 41, 5, 60, 74, 87, 90, 101, null, '', false, NaN, '12']
 
 const seedEvenAndOdd = (numberList) => {
   const evenNumbers = []
   const oddNumbers = []
 
   for (let index = 0; index < numberList.length; index++) {
-    if (!numberList[index]) {
+    const element = numberList[index]
+
+    if (!element || typeof element !== 'number') {
       continue
-    } else if (numberList[index] % 2 === 0) {
-      evenNumbers.push(numberList[index])
+    } else if (element % 2 === 0) {
+      evenNumbers.push(element)
     } else {
-      oddNumbers.push(numberList[index])
+      oddNumbers.push(element)
     }
   }
   return { evenNumbers, oddNumbers } // lo sacaria con un objeto pero como no lo hemos visto lo he hecho asi
