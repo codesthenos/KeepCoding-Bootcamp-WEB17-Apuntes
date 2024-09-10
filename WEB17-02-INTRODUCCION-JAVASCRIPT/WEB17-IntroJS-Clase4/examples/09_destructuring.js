012ef63d3dff4504f5fa9e086a949b3ecded6987
codesthenos <codesthenos@gmail.com>
2024-09-10 22:31:02 +0200

    Incluyo la destructuracion de arrays

--- WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase4/examples/09_destructuring.js
+++ WEB17-02-INTRODUCCION-JAVASCRIPT/WEB17-IntroJS-Clase4/examples/09_destructuring.js
@@ -15,6 +15,7 @@ console.log('Destructuring author:', author)
 console.log('Destructuring year:', year)
 
 // array of objects
+// no destructuring
 const books = [
   { title: '1984', author: 'George Orwell', year: 1949 },
   {
@@ -31,3 +32,37 @@ const books = [
   { title: 'Twilight', author: 'Stephenie Meyer', year: 2005 },
   { title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008 }
 ]
+const book1 = books[0]
+console.log(book1)
+
+// destructuring
+const [firstBook, secondBook, thirdBook, fourthBook, fifthBook, sixthBook] = [
+  { title: '1984', author: 'George Orwell', year: 1949 },
+  {
+    title: 'One Hundred Years of Solitude',
+    author: 'Gabriel García Márquez',
+    year: 1967
+  },
+  {
+    title: "Harry Potter and the Philosopher's Stone",
+    author: 'J.K. Rowling',
+    year: 1997
+  },
+  { title: 'The Da Vinci Code', author: 'Dan Brown', year: 2003 },
+  { title: 'Twilight', author: 'Stephenie Meyer', year: 2005 },
+  { title: 'The Hunger Games', author: 'Suzanne Collins', year: 2008 }
+]
+console.log(
+  'firstBook:',
+  fifthBook,
+  '\nsecondBook:',
+  secondBook,
+  '\nthirdBook:',
+  thirdBook,
+  '\nfourthBook:',
+  fourthBook,
+  '\nfifthBook:',
+  fifthBook,
+  '\nsixthBook:',
+  sixthBook
+)
