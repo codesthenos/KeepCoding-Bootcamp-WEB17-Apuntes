5f0f1df55d4cab0c45fd422850cd919f82f0c441
codesthenos <codesthenos@gmail.com>
2024-09-12 20:23:27 +0200

    Terminado ejercicio10.js sin corregir, y funciona apra ejemplo concreto, no valoro divisiones por 0 y eso

---
+++ 02-INTRODUCCION-JAVASCRIPT/Clase5/ejercicios/ejercicio10.js
@@ -0,0 +1,45 @@
+document.querySelector('body').innerHTML = `
+<h1 style='text-align: center';> ejercicio clase 3/4 WEB17-IntroJS-Clase3/ejercicios/ejercicio10.js</h1>
+`
+
+/*
+Ejericio Calculadora con closures
+
+Crear una calculadora con las siguientes funciones:
+*/
+
+const calculadora = () => {
+  let resultado = 0
+
+  const sumar = (input) => {
+    resultado = resultado + input
+  }
+
+  const restar = (input) => {
+    resultado = resultado - input
+  }
+
+  const multiplicar = (input) => {
+    resultado = resultado * input
+  }
+
+  const dividir = (input) => {
+    resultado = resultado / input
+  }
+
+  const total = () => {
+    return resultado
+  }
+
+  return { sumar, restar, multiplicar, dividir, total }
+}
+
+
+const miCalculadora = calculadora();
+
+console.log(miCalculadora.sumar(5)); // Debería imprimir undefined
+console.log(miCalculadora.restar(2)); // Debería imprimir undefined
+console.log(miCalculadora.multiplicar(4)); // Debería imprimir undefined
+console.log(miCalculadora.dividir(2)); // Debería imprimir undefined
+console.log(miCalculadora.sumar(10)); // Debería imprimir undefined
+console.log(miCalculadora.total()); // Debería imprimir 16
