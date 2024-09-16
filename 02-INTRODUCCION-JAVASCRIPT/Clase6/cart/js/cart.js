ab11514dbd0c8388721765196dde6d0cc04817e8
codesthenos <codesthenos@gmail.com>
2024-09-16 19:50:10 +0200

    copiado ejericio cart, Kevin procedera a resolverlo

---
+++ 02-INTRODUCCION-JAVASCRIPT/Clase6/cart/js/cart.js
@@ -0,0 +1,67 @@
+/**
+ * @typedef {Object} Discount
+ * @property {Product} product - The product the discount is applied to.
+ * @property {number} discount - The discount percentage applied to the product.
+ */
+const cart = () => {
+  // products = [{ name: 'T-shirt', price: 20, quantity: 2 }, { name: 'Cap', price: 5, quantity: 3 }];
+  /**
+   * Array of product objects in the cart.
+   * @type {Product[]}
+   */
+  let products = [];
+  // discounts = [{ product: { name: 'T-shirt', price: 20, quantity: 2 }, discount: 10 }];
+  /**
+   * Array of discount objects applied to products.
+   * @type {Discount[]}
+   */
+  let discounts = [];
+  
+  /**
+   * Adds a product to the cart.
+   * @param {Product} product - The product to add to the cart.
+   */
+  const addToCart = (product) => {};
+
+  /**
+   * Gets the list of products in the cart.
+   * @returns {Product[]} The list of products in the cart.
+   */
+  const getCart = () => {};
+
+  /**
+   * Gets the list of discounts applied to the cart.
+   * @returns {Discount[]} The list of discounts applied to the cart.
+   */
+  const getDiscounts = () => {};
+
+  /**
+   * Get the total price of the products in the cart.
+   * @returns {number} The total price of the products in the cart.
+   */
+  const getTotal = () => {};
+
+  /**
+   * Applies a discount to a product in the cart.
+   * @param {string} productName - The name of the product to apply the discount to.
+   * @param {string} discount - The discount percentage to apply.
+   * @throws {Error} If the product is not found in the cart.
+   */
+  const applyDiscount = (productName, discount) => {};
+
+  /**
+   * Removes a product from the cart and any discounts associated with it.
+   * @param {string} productName - The name of the product to remove.
+   */
+  const removeProduct = (productName) => {};
+
+  /**
+   * Removes a discount applied to a product in the cart.
+   * @param {string} productName - The name of the product to remove the discount from.
+   */
+  const removeDiscount = (productName) => {};
+
+  return { addToCart, getCart, applyDiscount, getDiscounts, removeDiscount, removeProduct, getTotal };
+};
+
+export default cart;
