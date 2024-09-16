d24b4e1581ec7270ddac7df91955b36d75e0f81b
codesthenos <codesthenos@gmail.com>
2024-09-16 19:42:34 +0200

    Inlcuyo la carpeta cart del repo de Kevin para seguir la ultima clase

--- 02-INTRODUCCION-JAVASCRIPT/Clase6/modules/main.js
+++ 02-INTRODUCCION-JAVASCRIPT/Clase6/modules/main.js
@@ -1,9 +1,8 @@
 document.querySelector('body').innerHTML = `
 <h1 style='text-align: center';> ejercicio clase 3/4 WEB17-IntroJS-Clase3/ejercicios/modules/main.js</h1>
 `
-// forma 1 (default)
+// forma 1 (default)       // forma 2 (sin default)
 import miCalculadora, { defaultValue, message, suma10 as suma } from './utils.js'
-// forma 2 (sin default)
 
 const count1 = miCalculadora()
 count1.sumar(12)
