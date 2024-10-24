a85a909f165facfce87849fc8d0621da21c296c2
codesthenos <codesthenos@gmail.com>
2024-10-24 19:54:14 +0200

    Usamos librerias debug y cross env para mostrar mas info en la consola cuando estamos en modo desarrollo

--- 05-DESARROLLO-BACKEND-NODEJS/clase3/expressNodeApp/server.js
+++ 05-DESARROLLO-BACKEND-NODEJS/clase3/expressNodeApp/server.js
@@ -1,19 +1,24 @@
 import express from 'express'
+import debugLib from 'debug'
 import { createServer } from 'node:http'
+
+const debug = debugLib('expressnodeapp:server')
+
+const port = process.env.PORT || 3200
 
 // creamos app con express
 const app = express()
 
 // definimos ruta raiz
 app.get('/', (req, res, next) => {
-  res.send('Hola codesthenos')
+  res.send('<h1>Hola codesthenos</h1>')
 })
 
-// arrancar el servidor
+// definimos un server 
 const server = createServer(app)
 
-server.on('listening', () => {
-  console.log('Servidor arrancado en\nhttp://localhost:3500')
-})
+// hacer que el servidor escuche el evento listening
+server.on('listening', () => { debug(`Servidor arrancado en\n\nhttp://localhost:${port}\n`) })
 
-server.listen(3500)
+// arrancar el servidor
+server.listen(port)
