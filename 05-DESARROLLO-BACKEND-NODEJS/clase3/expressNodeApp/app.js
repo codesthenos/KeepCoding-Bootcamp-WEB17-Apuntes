1c6388b8956933f4752847f28b733d679689b945
codesthenos <codesthenos@gmail.com>
2024-10-24 20:22:18 +0200

    middleware de error final

--- 05-DESARROLLO-BACKEND-NODEJS/clase3/expressNodeApp/app.js
+++ 05-DESARROLLO-BACKEND-NODEJS/clase3/expressNodeApp/app.js
@@ -5,6 +5,14 @@ const app = express()
 
 // definimos ruta raiz
 app.get('/', (req, res, next) => {
+  throw new Error('Error FORZADO')
   res.send('<h1>Hola codesthenos</h1>')
 })
+
+// middleware para manejar el error 'errorHandler'
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).send(err.message)
+  console.error(err.message)
+})
+
 export default app
