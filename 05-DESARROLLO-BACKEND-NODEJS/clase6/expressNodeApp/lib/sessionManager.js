c0a3f05b9e598f9eea35c550714c9c4942ea4ca5
codesthenos <codesthenos@gmail.com>
2024-10-31 21:54:08 +0100

    Usamos npm i connect-mongo para guardar las sesiones de los usuarios en la base de datos

--- 05-DESARROLLO-BACKEND-NODEJS/clase6/expressNodeApp/lib/sessionManager.js
+++ 05-DESARROLLO-BACKEND-NODEJS/clase6/expressNodeApp/lib/sessionManager.js
@@ -1,4 +1,5 @@
 import session from 'express-session'
+import MongoStore from 'connect-mongo'
 import { SESSION_SECRET } from '../secret.js'
 
 // middleware para gestionar sesiones
@@ -7,9 +8,13 @@ export const sessionMiddleware = session({
   secret: SESSION_SECRET, // hace que el manejo de las sesiones sea seguro
   saveUninitialized: true, // hace que las sesiones que no han hecho peticiones esten en la Store
   resave: false, // fuerza que una sesion se guarde aunque la sesion no se haya modificado durante la peticion
-  cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 }
+  cookie: { maxAge: 1000 * 60 * 60 * 24 * 3 },
+  // Las sesiones las guardamos en MongoDB
+  store: MongoStore.create({
+    mongoUrl: 'mongodb://127.0.0.1:27017/cursonode'
+  })
 })
-
+// middleware para linkear la info de la sesion a la VIEW
 export function sessionViewsMiddleware (req, res, next) {
   res.locals.session = req.session
   next()
