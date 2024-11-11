fa29a7cdc0275375cf3e46830e0eeabc18f61f4c
codesthenos <codesthenos@gmail.com>
2024-11-11 22:47:11 +0100

    Incluyo in miniscript para ver los diferentes metodos de acceder a elementos del DOM crearlos e incluirlos, poner texto en ellos

--- 06-DESARROLLO-FRONTEND-JAVASCRIPT/clase1/index.js
+++ 06-DESARROLLO-FRONTEND-JAVASCRIPT/clase1/index.js
@@ -5,3 +5,6 @@ const a = 5
 console.log('a = ' + a)
 
 export const b = 10
+// Si ejecuto esto con node, falla, si lo ejecuto en el browser no
+console.log(document)
+// console.log(process) --> esto en el browser falla con node no
