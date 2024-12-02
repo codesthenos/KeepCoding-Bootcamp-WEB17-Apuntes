3dee16feb26c922b9b618a5b3dd31e346864efaf
codesthenos <codesthenos@gmail.com>
2024-12-02 20:32:03 +0100

    ejamplo usando luxon copiando su archivo en nuestro proyecto, usandolo como el cdn en el caso de loadsh pero en nuestra carpeta en vez de a tgraves del cdn

--- 07-FRONTEND-PRO/clase1/examples/librerias/index.js
+++ 07-FRONTEND-PRO/clase1/examples/librerias/index.js
@@ -2,3 +2,6 @@ const array = ['a', 'b', 'c', 'd']
 // lodash _.chunk return a new array para usar lodash _.
 const array2 = _.chunk(array, 2)
 console.log(array, array2)
+// luxon
+const date = luxon.DateTime.now().toString()
+console.log(date)
