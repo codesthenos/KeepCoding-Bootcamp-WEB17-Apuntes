269d24739ac50d9029e8939bb5ada917b852ec8c
codesthenos <codesthenos@gmail.com>
2024-12-05 23:19:48 +0100

    cerramos viendo lo basico de la config de tailwind

---
+++ 07-FRONTEND-PRO/clase3/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./parcel-harry-potter/src/**/*.{html,js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
