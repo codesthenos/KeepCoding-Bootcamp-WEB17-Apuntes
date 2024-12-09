c2062d265ace7732e87832aa349a732f149301ae
codesthenos <codesthenos@gmail.com>
2024-12-09 21:47:50 +0100

    preparo el ejercicio 4 que parte del 3

--- 08-WEB-COMPONENTS/clase1/1.4_Shadow-DOM/digital-clock/exercise/digital-clock.js
+++ 08-WEB-COMPONENTS/clase1/1.4_Shadow-DOM/digital-clock/exercise/digital-clock.js
@@ -3,3 +3,24 @@
   2- Each second, we must calculate the time and update the component HTML
   3- Maybe we should use custom-properties...
 */
+class DigitalClock extends HTMLElement {
+  constructor () {
+    super()
+  }
+  connectedCallback () {
+    setInterval(() => {
+      this.innerHTML = this.getTime()
+    }, 1000)
+  }
+  getTime () {
+    const date = new Date()
+    const hours = date.getHours()
+    const minutes = date.getMinutes()
+    const seconds = date.getSeconds()
+
+    return `<h2>${hours}:${minutes}:${seconds}</h2>`
+  }
+}
+window.customElements.define('digital-clock', DigitalClock)
+
+const digitalClock = new DigitalClock()
