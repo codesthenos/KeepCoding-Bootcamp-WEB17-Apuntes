4295c018c4091bd9868690158850203f7558b6d3
codesthenos <codesthenos@gmail.com>
2025-01-08 20:40:16 +0100

    terminamos de jugar con los archivos de publisher y consumer y rabbitmq de esta forma aislada, no se si lo meteremos en nodeapp

--- 09-DESARROLLO-BACKEND-AVANZADO-NODEJS/clase5/examples/example-rabbitmq/consumer.js
+++ 09-DESARROLLO-BACKEND-AVANZADO-NODEJS/clase5/examples/example-rabbitmq/consumer.js
@@ -14,11 +14,15 @@ channel.assertQueue(QUEUE_NAME, {
   durable: true // la cola de mensajes sobrevive a reinicios del BROKER (si se reinicia el servidor de RabbitMQ), defaults to true
 })
 
+// cuantos mensajes es capaz este consumidor de procesar en paralelo
+channel.prefetch(1)
+
 // suscribirnos a la cola
-channel.consume(QUEUE_NAME, message => {
+channel.consume(QUEUE_NAME, async message => {
   const payload = JSON.parse(message.content.toString())
   // simulamos que hacemos un trabajo con el mensaje
   console.log(payload.tarea)
+  await new Promise(resolve => setTimeout(resolve, 100)) // con el prefecth(1) consumo un mensaje maximo por cada 3 segundos
   // (acknowledgement) avisamos al BROKER de que hemos terminado con el mensaje para poder ir al siguiente
   channel.ack(message)
 })
--- 09-DESARROLLO-BACKEND-AVANZADO-NODEJS/clase5/examples/example-rabbitmq/publisher.js
+++ 09-DESARROLLO-BACKEND-AVANZADO-NODEJS/clase5/examples/example-rabbitmq/publisher.js
@@ -14,14 +14,22 @@ await channel.assertExchange(EXCHANGE_NAME, 'direct', {
   durable: true // el exchange sobrevive a reinicios del BROKER (si se reinicia el servidor de RabbitMQ), defaults to true
 }) // asegura de que existe un exchange y si no existe lo crea
 
+let keepSending = true
+
 // publicar un mensaje
 while (true) {
   const message = {
     tarea: 'enviar un email ' + Date.now()
   }
-  
-  channel.publish(EXCHANGE_NAME, '*', Buffer.from(JSON.stringify(message)))
+
+  // verificar si puedo enviar mas mensajes o tengo que esperar
+  if (!keepSending) {
+    await new Promise(resolve => channel.on('drain', resolve))
+  }
+
+  // returns true or false si el buffer esta lleno, si false, emit un evento 'drain' cuando esta disponible
+  keepSending = channel.publish(EXCHANGE_NAME, '*', Buffer.from(JSON.stringify(message)))
 
   console.log(message.tarea)
-  await new Promise(resolve => setTimeout(resolve, 500))
+  await new Promise(resolve => setTimeout(resolve, 1000))
 }
