78acc9f36a2d7c45a251a948ea03c4d6034bc13e
codesthenos <codesthenos@gmail.com>
2025-01-20 22:19:44 +0100

    terminamos con el ejemplo para ver sintaxis y vamos a empezar con TDD

--- 10-TDD-JAVASCRIPT/clase1/examples/sum.test.js
+++ 10-TDD-JAVASCRIPT/clase1/examples/sum.test.js
@@ -6,15 +6,8 @@ describe('Funcion suma', () => {
   })
 
   test.only('sumar 1 + 2 es igual a 3', () => {
+    expect(suma(1, 2)).toBeGreaterThan(2)
     expect(suma(1, 2)).toBe(3)
-  })
-  
-  it('sumar 1 + -1 es igual a 0', () => {
-    expect(suma(1, -1)).toBe(0)
-  })
-
-  it('Sumar 1.100000 + 2.230404 sea igual a 3.330404', () => {
-    expect(suma(1.100000, 2.230404)).toBeCloseTo(3.330404, 6)
   })
 
   it.only('Sumar 0.1 + 0.2 sea igual a 0.3', () => {
@@ -27,5 +20,17 @@ describe('Funcion suma', () => {
 
   it('Sumar 0.1 + 0.2 sea igual a 0.3', () => {
     expect(suma(0.1, 0.2)).toBeCloseTo(0.3, 16)
+  })
+})
+
+describe.only('Funcion sum con numeros negativos', () => {
+  it('sumar 1 + -1 es igual a 0', () => {
+    expect(suma(1, -1)).not.toBeGreaterThan(0)
+  })
+})
+
+describe.only('Funcion sum con numeros decimales', () => {
+  it('Sumar 1.100000 + 2.230404 sea igual a 3.330404', () => {
+    expect(suma(1.100000, 2.230404)).toBeCloseTo(3.330404, 6)
   })
 })
