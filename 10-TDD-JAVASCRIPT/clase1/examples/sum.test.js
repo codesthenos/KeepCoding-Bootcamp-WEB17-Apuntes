cee0c3d59efc7bdd6c56166760736bb8d0a3294c
codesthenos <codesthenos@gmail.com>
2025-01-20 22:34:25 +0100

    seguimos

--- 10-TDD-JAVASCRIPT/clase1/examples/sum.test.js
+++ 10-TDD-JAVASCRIPT/clase1/examples/sum.test.js
@@ -5,12 +5,12 @@ describe('Funcion suma', () => {
     expect(suma(0, 0)).toEqual(0)
   })
 
-  test.only('sumar 1 + 2 es igual a 3', () => {
+  test('sumar 1 + 2 es igual a 3', () => {
     expect(suma(1, 2)).toBeGreaterThan(2)
     expect(suma(1, 2)).toBe(3)
   })
 
-  it.only('Sumar 0.1 + 0.2 sea igual a 0.3', () => {
+  it('Sumar 0.1 + 0.2 sea igual a 0.3', () => {
     expect(suma(0.1, 0.2)).toBeCloseTo(0.3, 1)
   })
 
@@ -23,14 +23,32 @@ describe('Funcion suma', () => {
   })
 })
 
-describe.only('Funcion sum con numeros negativos', () => {
+describe('Funcion sum con numeros negativos', () => {
   it('sumar 1 + -1 es igual a 0', () => {
     expect(suma(1, -1)).not.toBeGreaterThan(0)
   })
 })
 
-describe.only('Funcion sum con numeros decimales', () => {
+describe('Funcion sum con numeros decimales', () => {
   it('Sumar 1.100000 + 2.230404 sea igual a 3.330404', () => {
     expect(suma(1.100000, 2.230404)).toBeCloseTo(3.330404, 6)
+  })
+})
+
+describe.only('Testear con valores no numericos', () => {
+  it('Sumar 1 + a debe devolver un error', () => {
+    expect(() => suma(1, 'a')).toThrow() // => toThrow() lanza error
+  })
+
+  it('Sumar a + 1 debe devolver un error', () => {
+    expect(() => suma('a', 1)).toThrow()
+  })
+
+  it('Sumar a + a debe devolver un error', () => {
+    expect(() => suma('a', 'a')).toThrow()
+  })
+
+  it('Sumar algo que no sea un numero debe devolver un error', () => {
+    expect(() => suma('b', 'b')).toThrow()
   })
 })
