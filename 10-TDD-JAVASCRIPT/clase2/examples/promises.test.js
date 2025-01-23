3a0988e862f9a39ecad6990ddf35c254afd47cf9
codesthenos <codesthenos@gmail.com>
2025-01-23 21:25:45 +0100

    promises con callback done()

--- 10-TDD-JAVASCRIPT/clase2/examples/promises.test.js
+++ 10-TDD-JAVASCRIPT/clase2/examples/promises.test.js
@@ -17,3 +17,24 @@ describe("Async / Await style", () => {
     }
   })
 })
+describe('Callback style', () => {
+  it ('Llamada a successRequest debe devolver un status 200', (done) => {
+    expect.assertions(2)
+    // para usar then
+    successRequest().then(res => {
+      expect(res.status).toBe(200)
+      expect(res.statusText).toMatch(/OK/)
+      // uso done()
+      done()
+    })
+  })
+  it.only('Llamada a errorNotFoundRequest debe devolver un status 404', (done) => {
+    expect.assertions(3)
+    errorNotFoundRequest().catch(error => {
+      expect(error.response.status).toBe(404)
+      expect(error.response.statusText).toMatch(/OT FOU/)
+      expect(error.message).toMatch(/404/)
+      done()
+    })
+  })
+})
