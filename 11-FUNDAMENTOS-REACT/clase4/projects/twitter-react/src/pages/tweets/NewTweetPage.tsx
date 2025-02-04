61dd37a8705fcbea00f5b709c6b38aa966f5a8bd
codesthenos <codesthenos@gmail.com>
2025-02-04 20:31:19 +0100

    tengo que poner al dia el repo, vemos el forwardRef

--- 11-FUNDAMENTOS-REACT/clase4/projects/twitter-react/src/pages/tweets/NewTweetPage.tsx
+++ 11-FUNDAMENTOS-REACT/clase4/projects/twitter-react/src/pages/tweets/NewTweetPage.tsx
@@ -1,38 +1,60 @@
-import Layout from "../../components/layout/Layout";
-import Button from "../../components/shared/Button";
-import Photo from "../../components/shared/Photo";
-import Textarea from "../../components/shared/Textarea";
-import "./NewTweetPage.css";
+import { useEffect, useRef, useState } from 'react'
+import Page from '../../components/layaout/Page'
+import './NewTweetPage.css'
 
-const MAX_CHARACTERS = 140;
+const MAX_CHARACTERS = 140
 
 function NewTweetPage() {
+  const [content, setContent] = useState('')
+  const renders = useRef(0)
+  const timeout = useRef<number | undefined>(undefined)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  renders.current++
+  useEffect(() => {
+    timeout.current = setTimeout(() => {
+      console.log('timeout')
+    }, 10000)
+
+    return () => {
+      clearTimeout(timeout.current)
+    }
+  }, [])
+  console.log(timeout.current)
+
+  useEffect(() => {
+    console.log(buttonRef.current)
+  }, [])
   return (
-    <Layout title="What are you thinking?">
-      <div className="newTweetPage">
+    <Page title='What are you thinking?'>
+      <div className='newTweetPage'>
         <div>
           <Photo />
         </div>
-        <form className="newTweetPage-form">
+        <form className='newTweetPage-form'>
           <Textarea
-            className="newTweetPage-textarea"
+            className='newTweetPage-textarea'
             placeholder="Hey! What's up!"
             maxLength={MAX_CHARACTERS}
+            value={content}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setContent(event.target.value)
+            }}
           />
-          <div className="newTweetPage-footer">
-            <span className="newTweetPage-characters"></span>
+          <div className='newTweetPage-footer'>
+            <span className='newTweetPage-characters'></span>
             <Button
-              type="submit"
-              className="newTweetPage-submit"
-              $variant="primary"
+              type='submit'
+              className='newTweetPage-submit'
+              $variant='primary'
+              ref={buttonRef}
             >
               Let's go!
             </Button>
           </div>
         </form>
       </div>
-    </Layout>
-  );
+    </Page>
+  )
 }
 
-export default NewTweetPage;
+export default NewTweetPage
